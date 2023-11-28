97e2939379b067e4b42043795587cdb713e234b2
AmirulApon <apon11@cse.pstu.ac.bd>
2023-11-28 23:25:30 +0600

    remove some defualt test case and add post test

--- tests/Feature/PostControllerTest.php
+++ tests/Feature/PostControllerTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Post;
 
 class PostControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
@@ -16,5 +18,81 @@ class PostControllerTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * Test the creation of a new post.
+     *
+     * @return void
+     */
+    public function testCreatePost()
+    {
+        $response = $this->post(route('posts.store'), [
+            'title' => 'Test Post',
+            'content' => 'This is a test post content.',
+            'author' => 'Test Author',
+        ]);
+
+        $response->assertStatus(302); // 302 is the status code for a redirect
+        $response->assertRedirect(route('posts.index'));
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Test Post',
+            'content' => 'This is a test post content.',
+            'author' => 'Test Author',
+        ]);
+    }
+
+    /**
+     * Test the update of an existing post.
+     *
+     * @return void
+     */
+    public function testUpdatePost()
+    {
+        $post = Post::create([
+            'title' => 'Original Title',
+            'content' => 'Original Content',
+            'author' => 'Original Author',
+        ]);
+
+        $response = $this->put(route('posts.update', $post->id), [
+            'title' => 'Updated Title',
+            'content' => 'Updated Content',
+            'author' => 'Updated Author',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('posts.index'));
+
+        $this->assertDatabaseHas('posts', [
+            'id' => $post->id,
+            'title' => 'Updated Title',
+            'content' => 'Updated Content',
+            'author' => 'Updated Author',
+        ]);
+    }
+
+    /**
+     * Test the deletion of a post.
+     *
+     * @return void
+     */
+    public function testDeletePost()
+    {
+        $post = Post::create([
+            'title' => 'Post to be deleted',
+            'content' => 'Content to be deleted',
+            'author' => 'Author to be deleted',
+        ]);
+
+        $response = $this->delete(route('posts.destroy', $post->id));
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('posts.index'));
+
+        $this->assertDatabaseMissing('posts', [
+            'id' => $post->id,
+        ]);
     }
 }
